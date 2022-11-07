13713982a1ef19bb21f4ea134b891688884d3955
roldanmoncada <roldan.h.moncada@gmail.com>
2022-11-06 19:25:18 -0700

    created all the questionsgit add . going to define them at the top of my script file and then call them beneath it with every other function. figure this would be the easiest way although I have seen that sometimes it works well to have your questions in a separate JS file. Going to try this way before going with that. 6 questions for 60 seconds seems decent to me.

---
+++ script.js
@@ -0,0 +1,66 @@
+// creating questions first.  //
+
+let quizQuestions = [
+    {
+        question: "Commonly used data types DO NOT include:",
+        choices: [
+            "strings",
+            "booleans",
+            "alerts",
+            "numbers"
+        ],
+        answer: "alerts"
+
+    },
+    {
+        question: "The condition in an if/else statement is enclosed within ____.",
+        choices: [
+            "quotes",
+            "curly brackets",
+            "parentheses",
+            "square brackets"
+        ],
+        answer: "parentheses"
+
+    },
+    {
+        question: "Arrays in JavaScript can be used to store ____.",
+        choices: [
+            "numbers and strings",
+            "other arrays",
+            "booleans",
+            "all of the above"
+        ],
+        answer: "all of the above"
+    },
+    {
+        question: "String values must be enclosed within ____ when being assigned to variables",
+        choices: [
+            "commas",
+            "curly brackets",
+            "quotes",
+            "parentheses"
+        ],
+        answer: "quotes" 
+    },
+    {
+        question: "What JavaScript method allows you to join two or more arrays?",
+        choices: [
+            "join()",
+            "concat()",
+            "joinArray()",
+            "combine()"
+        ],
+        answer: "concat()"
+    },
+    {
+        question: "How do you retrieve an item from local storage?",
+        choices: [
+            "localStorage.get",
+            "localStorage.getItem('')",
+            "localstorage.getitem('')",
+            "local.storage.get.item ('')"
+        ],
+        answer: "localStorage.getItem('')"
+    }
+];
